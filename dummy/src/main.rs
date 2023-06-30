0c729710ea619cacf3356a0a2203be7590cee0b5
Kamil Koczurek <koczurekk@gmail.com>
2023-06-30 10:07:15 +0200

    move gpu impl to gpu/ and create empty self-test in dummy/

---
+++ dummy/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let sys_info = ya_self_test::system_info();
+    print!("{}", sys_info);
+}
--- gpu/src/main.rs
+++ gpu/src/main.rs
@@ -1,5 +1,5 @@
 fn main() -> anyhow::Result<()> {
-    let sys_info = ya_self_test::system_info()?;
+    let sys_info = ya_self_test_gpu::system_info()?;
 
     print!("{}", sys_info);
 
