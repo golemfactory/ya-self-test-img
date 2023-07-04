d370d3339038b65b49435c5c1be344d57522f93b
Przemyslaw Walski <pwalski@users.noreply.github.com>
2023-07-05 01:16:19 +0200

    File output cmdline param

--- dummy/src/main.rs
+++ dummy/src/main.rs
@@ -1,4 +1,14 @@
+use std::{env, fs};
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
     let sys_info = ya_self_test::system_info();
-    print!("{}", sys_info);
+
+    if args.len() == 2 {
+        fs::write(&args[1], sys_info.to_string())
+            .expect("Unable to write file");
+    } else {
+        print!("{}", sys_info);
+    }
 }
