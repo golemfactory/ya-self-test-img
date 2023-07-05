cfdb835d5c6802096c8abbec7e0b155c427a2366
Przemyslaw Walski <pwalski@users.noreply.github.com>
2023-07-05 05:32:06 +0200

    Formatting

--- dummy/src/main.rs
+++ dummy/src/main.rs
@@ -6,8 +6,7 @@ fn main() {
     let sys_info = ya_self_test::system_info();
 
     if args.len() == 2 {
-        fs::write(&args[1], sys_info.to_string())
-            .expect("Unable to write file");
+        fs::write(&args[1], sys_info.to_string()).expect("Unable to write file");
     } else {
         print!("{}", sys_info);
     }
