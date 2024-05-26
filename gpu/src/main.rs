59196952101f568cf6ef03dc5a6a448fdf4c30da
Przemysław Krzysztof Rekucki <przemyslaw.rekucki@golem.network>
2024-05-26 11:36:07 +0200

    fmt

--- gpu/src/main.rs
+++ gpu/src/main.rs
@@ -1,10 +1,10 @@
-use std::{env, fs};
-use std::error::Error;
 use golem_gpu_info::GpuDetectionBuilder;
 use serde_json::json;
+use std::error::Error;
+use std::{env, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let arg = env::args_os().skip(1).next();
+    let arg = env::args_os().nth(1);
     let gpu = GpuDetectionBuilder::default()
         .force_cuda()
         .unstable_props()
@@ -14,7 +14,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let sys_info = json!({"gpu": gpu});
 
     if let Some(path) = arg {
-        fs::write(&path, sys_info.to_string())?
+        fs::write(path, sys_info.to_string())?
     } else {
         print!("{}", sys_info);
     }
